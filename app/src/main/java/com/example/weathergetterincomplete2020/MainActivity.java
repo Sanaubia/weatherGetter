1966b1ba41fd9fde399ace0e1d111d1f6785166b
Sanaubia <Sanaubia@gmail.com>
2020-03-02 18:13:43 +0200

    shared preferences added

--- app/src/main/java/com/example/weathergetterincomplete2020/MainActivity.java
+++ app/src/main/java/com/example/weathergetterincomplete2020/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weathergetterincomplete2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,18 +10,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, WeatherEngine.WeatherDataAvailableInterface {
 
     WeatherEngine engine = new WeatherEngine(this);
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.button).setOnClickListener(this);
+
+        Loadshared();
+        updateView();
     }
 
 
@@ -31,12 +39,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected void updateUI()
     {
+        EditText edit = (EditText) findViewById(R.id.editText);
         TextView temperatureTextView = (TextView) findViewById(R.id.textView);
         String formatted = String.format(getString(R.string.temp), engine.getTemperature());
 
         temperatureTextView.setText(formatted);
         ImageView img = (ImageView) findViewById(R.id.imageView);
         Picasso.with(this).load("http://openweathermap.org/img/w/" + engine.getIconId() + ".png").into(img);
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, edit.getText().toString());
+
+        editor.apply();
+
+        Toast.makeText(this, "data saved", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -48,4 +65,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+    public void Loadshared(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+
+    }
+
+    public void updateView(){
+        EditText editor = (EditText) findViewById(R.id.editText);
+        editor.setText(text);
+    }
 }
+
